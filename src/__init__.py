5da60f9742cd5747743f54d5da1c07ffaa7e9351
nanakenashi <nanakenashi@gmail.com>
2016-09-01 00:40:48 +0900

    Modified config setting

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from src.config import choise_config
+
+environment = os.getenv('FLASK_ENV', 'development')
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('src.config')
+app.config.from_object(choise_config(environment))
 app.config.from_pyfile('config.cfg', silent=True)
 db = SQLAlchemy(app)
 
--- src/config.py
+++ src/config.py
@@ -1,1 +1,19 @@
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+class BaseConfig:
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+def choise_config(environment):
+    if environment == 'production':
+        config = 'src.config.ProductionConfig'
+    else:
+        config = 'src.config.DevelopmentConfig'
+
+    return config
